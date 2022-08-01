11f40a58db5d0334a28ef6541d78859a209fe924
Julia Peshkova <peshkova.julia@gmail.com>
2022-08-01 16:18:26 +0700

    pr 14.5 new host

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 const app = express();
 
